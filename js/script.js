90b280ffed10643ef0d9b391b6b8de951aea1c85
Fortu-42 <fortunatoherrerap@gmail.com>
2017-12-09 13:51:45 -0400

    almost

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 (function($) {
     "use strict";
 
+    window.sr = ScrollReveal({ reset: true });
+    
+
+    $('body').scrollspy({
+        target: '#my-navbar',
+        offset:300
+    });
+
     $('.nav-item').click(function(e) {
-        
                 $('.nav-item.active').removeClass('active');
-        
-                var $parent = $this.item();
-                $parent.addClass('active');
-                e.preventDefault();
+                $(this).addClass('active');
             });
 
 
@@ -15,6 +19,21 @@
         $('.fadein div:gt(0)').hide();
         setInterval(function(){$('.fadein > :first-child').fadeOut().next('div').fadeIn().end().appendTo('.fadein');}, 7000);
     });
+
+    sr.reveal('#about', { 
+        origin: 'bottom', 
+        duration: 2500 
+      });
+
+      sr.reveal('.card', { 
+        origin: 'bottom', 
+        duration: 2500 
+      });
+
+
+      
+
+    
 
 
 
